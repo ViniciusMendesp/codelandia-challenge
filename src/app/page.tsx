830e3d33bade725662d963cdde5b5bed7edae621
ViniciusMendesp <vmp_tech@outlook.com>
2024-02-11 18:18:09 -0300

    feat: :sparkles: metadata new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,27 @@ import { Card } from "@Aplications/components/Card";
 import cardData from "@Aplications/components/Card/cardData";
 import { Container } from "@Aplications/components/Container";
 import { Header } from "@Aplications/components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Codelandia Desafio",
+  description: "Primeiro desafio da comunidade Codelandia",
+  keywords:
+    "blog, desafio, codelandia, typescript, nextjs, javascript, tailwind",
+  creator: "Vinicius Mendes",
+  openGraph: {
+    type: "website",
+    url: "https://codelandia-challenge-one.vercel.app/",
+    title: "Codelandia Desafio",
+    description: "Primeiro desafio da comunidade Codelandia",
+    images: [
+      {
+        url: "/codelandia.png",
+        alt: "Codelandia",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
