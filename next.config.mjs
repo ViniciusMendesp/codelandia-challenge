75a53b0e3446c8e2d0362f81ae612c927ade5e63
ViniciusMendesp <vmp_tech@outlook.com>
2024-02-11 18:23:37 -0300

    feat: :sparkles: metadata config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  metadataBase: "https://codelandia-challenge-one.vercel.app/",
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${inter.className} font-SpaceGrotesk`}>{children}</body>
     </html>
   );
